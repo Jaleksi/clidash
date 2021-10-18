61233ba25390be95825af4d68af3a93dfb3322fb
Jaleksi <aleksi.jok@gmail.com>
2021-10-18 11:42:54 +0300

    fill zero data

--- dashcli/blocks/korona_block.py
+++ dashcli/blocks/korona_block.py
@@ -12,10 +12,40 @@ class KoronaBlock(urwid.LineBox):
 
     def update(self, main_loop, data=None):
         corona_data = fetch_data()
-        max_value = max(i['new_cases'] for i in corona_data)
+        data_values = [day['new_cases'] for day in corona_data]
+        print(data_values)
+        data_values = fill_zero_values_in_data(data_values)
+        print(data_values)
+        max_value = max(data_values)
 
-        cases = [day['new_cases'] for day in corona_data]
-        daily_cases = [(i,) for i in cases]
+        daily_cases = [(i,) for i in data_values]
         self.graph.set_data(daily_cases, max_value)
 
         main_loop.set_alarm_in(self.update_interval, self.update)
+
+
+
+def fill_zero_values_in_data(data):
+    zeroes_in_queue = []
+    last_numeric_value = 0
+
+    for i, val in enumerate(data):
+        if val == 0:
+            zeroes_in_queue.append(i)
+            if i != len(data) - 1:
+                continue
+
+        if not zeroes_in_queue:
+            last_numeric_value = val
+            continue
+
+        last_numeric_value = val if last_numeric_value == 0 else last_numeric_value
+        fill_step = (last_numeric_value - val) // (len(zeroes_in_queue) + 1)
+
+        for j, zero_index in enumerate(zeroes_in_queue):
+            data[zero_index] = last_numeric_value - fill_step * (j + 1)
+
+        last_numeric_value = val
+        zeroes_in_queue = []
+
+    return data
